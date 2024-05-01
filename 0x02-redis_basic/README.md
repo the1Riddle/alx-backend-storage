a9949ce9ce294da10fe3ff5a1c5b25baa2172025
the1Riddle <elvisotienomboya@gmail.com>
2024-05-01 15:12:33 +0300

    initial commit

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,18 @@
+# Redis basic
+
+## Resources
+
+**Install Redis on Ubuntu**
+
+```
+$ sudo apt-get -y install redis-server
+$ pip3 install redis
+$ sed -i "s/bind .*/bind 127.0.0.1/g" /etc/redis/redis.conf
+```
+
+**Use Redis in a container**
+
+- Redis server is stopped by default - when you are starting a container, you should start it with: 
+```service redis-server start```
+
+*End*
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+''' Implementing an expiring web cache and tracker
+'''
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+'''Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''Wrapper function for caching output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the URL content after caching the request's response
+    '''
+    return requests.get(url).text
